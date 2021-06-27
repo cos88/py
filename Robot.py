66d8d83a5cd7241b28337df1ef353acc5e364840
cos88 <36196574+cos88@users.noreply.github.com>
2021-06-27 22:06:11 +0100

    Create Robot.py

---
+++ Robot.py
@@ -0,0 +1,27 @@
+class Robot:
+
+    def __init__(self):
+
+        self.known_words = {'robot','computer','inteligence',
+            'me', 'do', 'already', 'word', 'thank', 'for', 'i', 'understand',
+            'know', 'teaching', 'not', 'input', 'the', 'you'}
+
+    def learn_word(self, word):
+
+        low = word.lower()
+
+        if word.isalpha():
+
+            if low in self.known_words:
+
+                return 'I already know the word {}'.format(word)
+
+            self.known_words.add(low)
+
+            return 'Thank you for teaching me {}'.format(word)
+
+        return 'I do not understand the input'
+
+r = Robot().learn_word('robot')
+
+print(r)
