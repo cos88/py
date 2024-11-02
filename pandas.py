434a3def79ef574adad25731e120351002c92e8b
cos88 <36196574+cos88@users.noreply.github.com>
2024-11-02 18:06:04 +0200

    Delete pandas.py

--- pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-days = [['Mon',20],['Tue',21],['Wed',22],['Thu',23],['Fri',24],['Sat',25],['Sun',26]]
-
-df = pd.DataFrame(days,index=[1,2,3,4,5,6,7],columns =['Days','Date'])
-
-print(df)
